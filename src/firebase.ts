9c5e579f2750ade7efd5ec912b485483d4a55136
Aaron Foster <159871892+AMFoster92@users.noreply.github.com>
2026-04-19 19:28:24 -0400

    fix npm errors

--- src/firebase.ts
+++ src/firebase.ts
@@ -23,8 +23,6 @@ function signInAuth(){
   return signInWithPopup(auth, provider)
     .then((result) => {
   // This gives you a Google Access Token. You can use it to access the Google API.
-  const credential = GoogleAuthProvider.credentialFromResult(result);
-  const token = credential?.accessToken;
   // The signed-in user info.
   const user = result.user;
   return user;
@@ -32,12 +30,8 @@ function signInAuth(){
   // ...
 }).catch((error) => {
   // Handle Errors here.
-  const errorCode = error.code;
-  const errorMessage = error.message;
   // The email of the user's account used.
-  const email = error.customData.email;
   // The AuthCredential type that was used.
-  const credential = GoogleAuthProvider.credentialFromError(error);
 
   console.log(error)
   // ...
